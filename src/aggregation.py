60893cf5ced33bee8c628f6c1c10548534f98ab8
Harold Rosales <h.rosales@estudiantes.matcom.uh.cu>
2020-11-18 21:35:55 -0500

    adding aggregation methods

--- src/aggregation.py
+++ src/aggregation.py
@@ -1,6 +1,7 @@
+from .set import fuzzySet
 
-def mamdani():
-    pass
+def mamdani(degree, fs: fuzzySet, z):
+    return min( degree, fs.func(z))
 
-def larsen():
-    pass
+def larsen(degree, fs: fuzzySet, z):
+    return degree * fs.func(z)
